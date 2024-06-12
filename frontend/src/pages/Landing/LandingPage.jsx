b751a0fc591a714e6c89a196a2103a967399b1ba
angelicagottlieb <angelicagottlieb@gmail.com>
2024-06-12 14:36:14 +0100

    Initial landing page - Angelica

---
+++ frontend/src/pages/Landing/LandingPage.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+import foodImage1 from "../../assets/image1.jpg";
+
+export const LandingPage = () => {
+  return (
+    <div className="landing">
+      <h1>Welcome to Sustainabite!</h1>
+      <p>
+        <Link to="/home" className="fridge-button">
+          <span className="default-text">What's in your fridge?</span>
+          <span className="hover-text">Check out all the amazing recipes you can make with what you've got available</span>
+        </Link>
+      </p>
+      <p>
+        <Link to="/signup">Create an account to save your favourite recipes and preferences</Link>
+      </p>
+      <img src={foodImage1} alt="Fridge" className="fridge-image" />
+    </div>
+  );
+};
